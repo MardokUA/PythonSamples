79761c104f4405caa3a84bee4d0b75aff1f873b2
Roman Laktionov <gbrfxe4177>
2017-08-24 19:15:21 +0300

    Third step

--- app/src/main/java/com/soleren/pythonsamples/activities/MainActivity.java
+++ app/src/main/java/com/soleren/pythonsamples/activities/MainActivity.java
@@ -5,14 +5,12 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.WindowManager;
 
-import com.google.android.gms.ads.AdRequest;
 import com.soleren.pythonsamples.R;
 import com.soleren.pythonsamples.adapters.MainAdapter;
 import com.soleren.pythonsamples.data.Const;
@@ -24,69 +22,64 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements MainActivityContract.View {
 
-    private ActionBarDrawerToggle drawerToggle;
-    private ActivityMainBinding binding;
-    private MainAdapter adapter;
-    private Toolbar toolbar;
-    private ArrayList<String> listTitles;
-    //names of navbar menu
-    private MainActivityPresenterImpl presenter;
-    private FragmentManager fm;
-    private AdRequest adRequest;
-    private boolean isScreenOn;
+    private ActivityMainBinding mDataBinding;
+    private Toolbar mToolbar;
+
+    private MainActivityPresenterImpl mActivityPresenter;
+    private FragmentManager mFragmentManager;
+    private MainAdapter mMainAdapter;
+
+    private ArrayList<String> mListTitles;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        initToolBar();
+        initFragmentManager(savedInstanceState);
+        initPresenter();
+        initAdapter();
+        bindAdapterToView();
+    }
 
-        fm = getSupportFragmentManager();
+    private void initToolBar() {
+        mToolbar = mDataBinding.toolbar;
+        setSupportActionBar(mToolbar);
+    }
+
+    private void initFragmentManager(Bundle savedInstanceState) {
+        mFragmentManager = getSupportFragmentManager();
         if (savedInstanceState != null) {
-            if (fm.getBackStackEntryCount() != 0) {
-                binding.mainActivityRecycler.setVisibility(View.GONE);
+            if (mFragmentManager.getBackStackEntryCount() != 0) {
+                mDataBinding.mainActivityRecycler.setVisibility(View.GONE);
             }
         }
-//        drawerToggle = new ActionBarDrawerToggle(this, binding.drawerLayout, R.string.open_drawer, R.string.close_drawer);
-        presenter = new MainActivityPresenterImpl(this);
-//        binding.drawerLayout.addDrawerListener(drawerToggle);
+    }
 
-        listTitles = presenter.getListTitles();
-        adapter = new MainAdapter(listTitles);
-        binding.mainActivityRecycler.setAdapter(adapter);
-        LinearLayoutManager manager = new LinearLayoutManager(this);
-        binding.mainActivityRecycler.setLayoutManager(manager);
+    private void initPresenter() {
+        mActivityPresenter = new MainActivityPresenterImpl(this);
+        mListTitles = mActivityPresenter.getListTitles();
+    }
 
-
-        adapter.setListener(new MainAdapter.AdapterListener() {
+    private void initAdapter() {
+        mMainAdapter = new MainAdapter(mListTitles);
+        mMainAdapter.setListener(new MainAdapter.AdapterListener() {
             @Override
             public void onClick(int position) {
-                presenter.selectFragment(position);
-                binding.mainActivityRecycler.setVisibility(View.GONE);
-                if (binding.adView != null)
-                    binding.adView.destroy();
+                mActivityPresenter.selectFragment(position);
+                mDataBinding.mainActivityRecycler.setVisibility(View.GONE);
+                if (mDataBinding.adView != null)
+                    mDataBinding.adView.destroy();
             }
         });
-        toolbar = binding.toolbar;
-        setSupportActionBar(toolbar);
-//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-//        getSupportActionBar().setHomeButtonEnabled(true);
+    }
 
-        //actions after clicking on navbar menu
-//        binding.navigtionView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-//            @Override
-//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-//                Log.d("!!!",item.getItemId()+"");
-//                presenter.selectFragment(item.getItemId());
-//                if(binding.adView != null)
-//                    binding.adView.destroy();
-//                return true;
-//            }
-//        });
-
-//        adRequest = new AdRequest.Builder().build();
-//        binding.adView.loadAd(adRequest);
+    private void bindAdapterToView() {
+        mDataBinding.mainActivityRecycler.setAdapter(mMainAdapter);
+        LinearLayoutManager manager = new LinearLayoutManager(this);
+        mDataBinding.mainActivityRecycler.setLayoutManager(manager);
     }
 
     @Override
@@ -95,87 +88,50 @@ public class MainActivity extends AppCompatActivity implements MainActivityContr
         outState.putBoolean("visible", true);
     }
 
-//    @Override
-//    public void onConfigurationChanged(Configuration newConfig) {
-//        super.onConfigurationChanged(newConfig);
-//        //change view of drawerToggle
-//        drawerToggle.onConfigurationChanged(newConfig);
-//    }
-
-
-//    @Override
-//    protected void onPostCreate(Bundle savedInstanceState) {
-//        super.onPostCreate(savedInstanceState);
-//        // sync  onRestoreInstanceState.
-//        drawerToggle.syncState();
-//    }
-
-
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        if (drawerToggle.onOptionsItemSelected(item))// let drawerToggle act
-//            return true;
-//        return super.onOptionsItemSelected(item);
-//    }
-
-
     @Override
     public void setActionBarTitle(String title) {
-        toolbar.setTitle(title);
+        mToolbar.setTitle(title);
     }
 
     @Override
     public void setActiveFragment(Fragment fragment) {
-
-        FragmentTransaction ft = fm.beginTransaction();
+        FragmentTransaction ft = mFragmentManager.beginTransaction();
         ft.replace(R.id.fragment_container, fragment)
                 .addToBackStack(null)
                 .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
                 .commit();
-
-
-//        getSupportFragmentManager().addOnBackStackChangedListener(new FragmentManager.OnBackStackChangedListener() {
-//            @Override
-//            public void onBackStackChanged() {
-//                Fragment currentBackStackFragment = getSupportFragmentManager().findFragmentByTag("MainFragment");
-//                if(currentBackStackFragment != null){
-//                    Log.d("!!!",currentBackStackFragment.toString());
-//                }
-//            }
-//        });
-//        binding.drawerLayout.closeDrawer(GravityCompat.START);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        if (binding.adView != null)
-            binding.adView.pause();
+        if (mDataBinding.adView != null)
+            mDataBinding.adView.pause();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        if (binding.adView != null)
-            binding.adView.resume();
+        if (mDataBinding.adView != null)
+            mDataBinding.adView.resume();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        presenter.destroy();
-        if (binding.adView != null)
-            binding.adView.destroy();
+        mActivityPresenter.destroy();
+        if (mDataBinding.adView != null)
+            mDataBinding.adView.destroy();
     }
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
         getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        if (fm != null) {
-            if (fm.getBackStackEntryCount() == 0) {
-                presenter.selectActionBarTitle(Const.TITLE);
-                binding.mainActivityRecycler.setVisibility(View.VISIBLE);
+        if (mFragmentManager != null) {
+            if (mFragmentManager.getBackStackEntryCount() == 0) {
+                mActivityPresenter.selectActionBarTitle(Const.TITLE);
+                mDataBinding.mainActivityRecycler.setVisibility(View.VISIBLE);
             }
         }
     }
--- app/src/main/java/com/soleren/pythonsamples/utils/XMLParser.java
+++ app/src/main/java/com/soleren/pythonsamples/utils/XMLParser.java
@@ -1,6 +1,5 @@
 package com.soleren.pythonsamples.utils;
 
-import android.content.Context;
 import android.text.TextUtils;
 
 import com.soleren.pythonsamples.application.PythonSamples;
@@ -12,15 +11,10 @@ import org.xmlpull.v1.XmlPullParserException;
 import java.io.IOException;
 import java.util.ArrayList;
 
-/**
- * Created by den on 2017-05-22.
- */
-
 public class XMLParser {
 
     private static XMLParser xmlParser = new XMLParser();
     private static int resourceId;
-    private ArrayList<Item> items;
 
     private XMLParser() {
     }
@@ -31,71 +25,39 @@ public class XMLParser {
     }
 
     public ArrayList<Item> parse() {
+
         XmlPullParser xpp = prepare(resourceId);
-        items = new ArrayList<>();
+        ArrayList<Item> items = new ArrayList<>();
         Item item = new Item();
         String tag = "";
+
         try {
             while (xpp.getEventType() != XmlPullParser.END_DOCUMENT) {
                 switch (xpp.getEventType()) {
                     case XmlPullParser.START_DOCUMENT:
-//                        Log.d("!!!", "Start");
                         break;
-
                     case XmlPullParser.START_TAG:
                         switch (xpp.getName()) {
-//                            case "id":
-//                                tag = "id";
-//                                break;
                             case "title":
                                 tag = "title";
                                 break;
                             case "content":
                                 tag = "content";
                                 break;
-//                            case "title_ru":
-//                                tag = "title_ru";
-//                                break;
-//                            case "title_en":
-//                                tag = "title_en";
-//                                break;
-//                            case "content_ru":
-//                                tag = "content_ru";
-//                                break;
-//                            case "content_en":
-//                                tag = "content_en";
-//                                break;
                             case "print":
                                 tag = "print";
                                 break;
-
                         }
                         break;
-
                     case XmlPullParser.TEXT:
                         if (!TextUtils.isEmpty(tag)) {
                             switch (tag) {
-//                                case "id":
-//                                    item.setId(xpp.getText());
-//                                    break;
                                 case "title":
                                     item.setTitle(xpp.getText());
                                     break;
                                 case "content":
                                     item.setContent(xpp.getText());
                                     break;
-//                                case "title_ru":
-//                                    item.setTitle_ru(xpp.getText());
-//                                    break;
-//                                case "title_en":
-//                                    item.setTitle_en(xpp.getText());
-//                                    break;
-//                                case "content_ru":
-//                                    item.setContent_ru(xpp.getText());
-//                                    break;
-//                                case "content_en":
-//                                    item.setContent_en(xpp.getText());
-//                                    break;
                                 case "print":
                                     item.setPrint(xpp.getText());
                                     break;
@@ -111,7 +73,6 @@ public class XMLParser {
                         }
                         if (!TextUtils.isEmpty(tag))
                             tag = "";
-
                         break;
                     default:
                         break;
@@ -119,8 +80,6 @@ public class XMLParser {
                 xpp.next();
             }
             return items;
-
-//            Log.d("!!!", "END_DOCUMENT");
         } catch (XmlPullParserException | IOException e) {
             e.printStackTrace();
         }
